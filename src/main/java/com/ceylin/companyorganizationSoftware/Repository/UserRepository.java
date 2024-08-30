932de69f218bc589fa63cc251d56e57d4ecbb4cb
ceylinerdogan <116516898+ceylinerdogan@users.noreply.github.com>
2024-08-30 18:30:44 +0300

    missing file added

--- src/main/java/com/ceylin/companyorganizationSoftware/Repository/UserRepository.java
+++ src/main/java/com/ceylin/companyorganizationSoftware/Repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ceylin.companyorganizationSoftware.Repository;
 
+import com.ceylin.companyorganizationSoftware.Model.Department;
 import com.ceylin.companyorganizationSoftware.Model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
         Optional<User> findById(Long id);
         Optional<User> findByEmail(String email);
         List<User> findAll();
+        Page<User> findByDepartment(Department department, Pageable pageable);
 }
 
