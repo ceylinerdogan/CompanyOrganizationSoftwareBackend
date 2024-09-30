86841ac12a5d4af4282cb6f5c99ebf00123ad93a
ceylinerdogan <116516898+ceylinerdogan@users.noreply.github.com>
2024-09-30 20:47:26 +0300

    authority change for update/delete

--- src/main/java/com/ceylin/companyorganizationSoftware/Config/SecurityConfig.java
+++ src/main/java/com/ceylin/companyorganizationSoftware/Config/SecurityConfig.java
@@ -33,7 +33,10 @@ public class SecurityConfig {
                         .requestMatchers("api/auth/**").permitAll()
                         .requestMatchers("api/user/upload-profile-picture").hasAnyAuthority("ADMIN", "MANAGER","USER") // Allow authenticated users to upload profile picture
                         .requestMatchers("api/user/profile").hasAnyAuthority("ADMIN", "MANAGER","USER")
-                        .requestMatchers("api/user/**").hasAnyAuthority("ADMIN", "MANAGER")  // Other user-related operations restricted to admin/manager
+                        .requestMatchers("api/user/**").hasAuthority("ADMIN")
+                        .requestMatchers("api/user/update/**").hasAnyAuthority("ADMIN", "MANAGER")  // Allow both ADMIN and MANAGER to update users
+                        .requestMatchers("api/user/delete/**").hasAnyAuthority("ADMIN", "MANAGER")  // Allow both ADMIN and MANAGER to delete users
+                        .requestMatchers("api/user/**").hasAnyAuthority("ADMIN", "MANAGER")
                         //.requestMatchers("api/user/update","api/user/update/**").hasAuthority("MANAGER")
                         //.requestMatchers("api/user/delete","api/user/delete/**").hasAuthority("MANAGER")
                         //.requestMatchers("api/user/belonging-department").hasAuthority("MANAGER")
